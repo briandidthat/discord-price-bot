0a2cebbd2b28f942f3ce46a9312fc1b7c9c59669
briandidthat <blora.dev@gmail.com>
2023-05-08 22:50:38 -0400

    simplify bot

--- bot.py
+++ bot.py
@@ -1,14 +1,32 @@
 import os
 
+import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-
-from client import Client
 
 # load environment variables
 load_dotenv()
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = Client()
+bot = commands.Bot(command_prefix="$", intents=intents)
 
-client.run(token=TOKEN)
+
+@bot.event
+async def on_ready():
+    print("price bot is online")
+
+
+@bot.command(name="spot", description="get the current spot price of a token")
+async def spot(ctx, symbol: str = commands.parameter(default="default")):
+    print(symbol)
+    if symbol == "default":
+        await ctx.send("You must provide a symbol. Try again.")
+        return
+    await ctx.send(f"you asked for {symbol}")
+
+
+bot.run(token=TOKEN)
