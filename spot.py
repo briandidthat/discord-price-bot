43e3d46ade18e714a52f85b1ebd2d17cab04f1e5
briandidthat <blora.dev@gmail.com>
2023-05-19 20:30:41 -0400

    statistics

--- spot.py
+++ spot.py
@@ -1,9 +1,10 @@
+import datetime
 import os
 
 import requests
 from dotenv import load_dotenv
 
-from util import SpotPrice, BatchRequest
+from util import SpotPrice, BatchRequest, Statistic
 
 # load environment variables
 load_dotenv()
@@ -12,7 +13,7 @@ price_server_url = os.getenv("PRICE_SERVER_URL")
 
 class SpotFetcher:
     @staticmethod
-    def get_spot_price(symbol: str, caller: str):
+    def get_spot_price(caller: str, symbol: str) -> SpotPrice:
         try:
             response = requests.get(f"{price_server_url}?symbol={symbol}", headers={"caller": caller})
             return SpotPrice(response.json())
@@ -20,7 +21,7 @@ class SpotFetcher:
             print(f"Http Exception occurred. {e}")
 
     @staticmethod
-    def get_historical_spot_price(symbol: str, date: str, caller: str):
+    def get_historical_spot_price(caller: str, symbol: str, date: str) -> SpotPrice:
         try:
             response = requests.get(f"{price_server_url}/historical?symbol={symbol}&date={date}",
                                     headers={"caller": caller})
@@ -29,7 +30,7 @@ class SpotFetcher:
             print(f"Http Exception occurred. {e}")
 
     @staticmethod
-    def get_batch_spot_price(symbols: list[str], caller: str):
+    def get_batch_spot_price(caller: str, symbols: list[str]) -> list[SpotPrice]:
         try:
             response = requests.get(f"{price_server_url}/batch", json={"requests": symbols}, headers={"caller": caller})
             data = response.json()
@@ -39,12 +40,25 @@ class SpotFetcher:
             print(f"Http Exception occurred. {e}")
 
     @staticmethod
-    def get_batch_historical_spot_price(batch_request: BatchRequest, caller: str):
+    def get_batch_historical_spot_price(caller: str, batch_request: BatchRequest) -> list[SpotPrice]:
         try:
             response = requests.get(f"{price_server_url}/historical/batch", json=batch_request.serialize(),
                                     headers={"caller": caller})
             data = response.json()
             responses = [SpotPrice(x) for x in data]
             return responses
+        except Exception as e:
+            print(f"Http Exception occurred. {e}")
+
+    @staticmethod
+    def get_price_statistics(caller: str, symbol: str, start_date: str, end_date: str = None):
+        try:
+            if end_date is None:
+                end_date = datetime.date.today()
+
+            response = requests.get(
+                f"{price_server_url}/statistics?{symbol}&startDate={start_date}&end_date={end_date}",
+                headers={"caller": caller})
+            return Statistic(response.json())
         except Exception as e:
             print(f"Http Exception occurred. {e}")
