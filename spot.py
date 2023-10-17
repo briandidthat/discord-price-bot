fe4643f81013c0182c14dcbbd0ef86139bd0cc4e
briandidthat <blora.dev@gmail.com>
2023-10-16 22:38:01 -0400

    Fix batch and statistics requests

--- spot.py
+++ spot.py
@@ -25,7 +25,7 @@ class SpotFetcher:
         request = Request(symbol, "", "")
         try:
             response = requests.post(f"{price_server_url}/spot", json=request.serialize(),
-                                     headers={"caller": caller, "Content-Type": "application/json"})
+                                     headers={"caller": caller})
             return SpotPrice(response.json())
         except Exception as e:
             print(f"Http Exception occurred. {e}")
@@ -43,8 +43,8 @@ class SpotFetcher:
     @staticmethod
     def get_batch_spot_price(caller: str, symbols: list[str]) -> list[SpotPrice]:
         try:
-            response = requests.get(f"{price_server_url}/spot/batch", json={"requests": symbols},
-                                    headers={"caller": caller})
+            response = requests.post(f"{price_server_url}/spot/batch", json={"requests": symbols},
+                                     headers={"caller": caller})
             data = response.json()
             responses = [SpotPrice(x) for x in data]
             return responses
@@ -54,8 +54,8 @@ class SpotFetcher:
     @staticmethod
     def get_batch_historical_spot_price(caller: str, batch_request: BatchRequest) -> list[SpotPrice]:
         try:
-            response = requests.get(f"{price_server_url}/spot/historical/batch", json=batch_request.serialize(),
-                                    headers={"caller": caller})
+            response = requests.post(f"{price_server_url}/spot/historical/batch", json=batch_request.serialize(),
+                                     headers={"caller": caller})
             data = response.json()
             responses = [SpotPrice(x) for x in data]
             return responses
@@ -64,13 +64,12 @@ class SpotFetcher:
 
     @staticmethod
     def get_price_statistics(caller: str, symbol: str, start_date: str, end_date: str = None):
+        if end_date is None:
+            end_date = datetime.date.today()
+        request = Request(symbol, start_date, end_date)
         try:
-            if end_date is None:
-                end_date = datetime.date.today()
-
-            response = requests.get(
-                f"{price_server_url}/spot/statistics?symbol={symbol}&startDate={start_date}&end_date={end_date}",
-                headers={"caller": caller})
+            response = requests.post(f"{price_server_url}/spot/statistics", json=request.serialize(),
+                                     headers={"caller": caller})
             return Statistic(response.json())
         except Exception as e:
             print(f"Http Exception occurred. {e}")
